08c79d849b14820445cbb1c555df6527b027b595
Petre Tudor <petre.tudor@protonmail.com>
2018-06-30 13:50:36 +0200

    added new temp container. wip

--- background.js
+++ background.js
@@ -1,45 +1,75 @@
+const tempContextName = 'temp';
+var tempContext;
+
+createTempContextIfNotExist();
+console.log('tempContext finale', tempContext);
+
+async function findContextByName(name, returnFirst = true){
+  const contexts = await browser.contextualIdentities.query({});
+  contexts.push({
+    cookieStoreId: 'firefox-default',
+    name: 'default'
+  });
+  let results = [];
+  
+  for (let context of contexts) {
+    if (context.name.toLowerCase().indexOf(name.toLowerCase()) > -1) {
+      if(returnFirst){
+        return context;
+      }
+      results.push({
+        content: context.name,
+        description: `Switch to container: ${context.name}`
+      })
+    }
+  }
+
+  return (results != []) ? results : faslse;
+}
+
+async function createTempContextIfNotExist(){
+  tempContext = await findContextByName(tempContextName);
+  console.log('tempContext createifnot', tempContext);
+  
+  if (!tempContext){
+    tempContext = await createTempContext();
+  }
+}
+
+function createTempContext() {
+  return browser.contextualIdentities.create({
+    name: "temp",
+    color: "purple",
+    icon: "briefcase"
+  })
+}
+
+
 browser.omnibox.setDefaultSuggestion({
   description: `Search for containers and switch to them (e.g. "co personal" or "co banking")`
 });
 
 browser.omnibox.onInputChanged.addListener(async (text, addSuggestions) => {
   const contexts = await browser.contextualIdentities.query({});
-  const result = [];
-  contexts.push({
-    name: "default",
-    description: `Switch to container: default`
-  });
-  for (let context of contexts) {
-    if (context.name.toLowerCase().indexOf(text.toLowerCase()) > -1) {
-      result.push({
-        content: context.name,
-        description: `Switch to container: ${context.name}`
-      })
-    }
-  }
-  addSuggestions(result);
+  
+  let results = await findContextByName(text, false)
+  addSuggestions(results);
 });
 
 browser.omnibox.onInputEntered.addListener(async (text, disposition) => {
-  const contexts = await browser.contextualIdentities.query({});
   const tabs = await browser.tabs.query({ currentWindow: true, active: true });
-  contexts.push({
-    cookieStoreId: 'firefox-default',
-    name: 'default'
-  });
 
-  for (let context of contexts) {
-    if (context.name.toLowerCase().indexOf(text.toLowerCase()) > -1) {
-      let tabCreateProperties = {
-        cookieStoreId: context.cookieStoreId,
-        index: tabs[0].index
-      };
-      if(tabs[0].url !== 'about:newtab') {
-        tabCreateProperties.url = tabs[0].url;
-      }
-      browser.tabs.create(tabCreateProperties);
-      browser.tabs.remove(tabs[0].id);
-      break
+  let context = await findContextByName(text);
+  if(context){
+    let tabCreateProperties = {
+      cookieStoreId: context.cookieStoreId,
+      index: tabs[0].index
+    };
+   
+    if (tabs[0].url !== 'about:newtab') {
+      tabCreateProperties.url = tabs[0].url;
     }
+    browser.tabs.create(tabCreateProperties);
+    browser.tabs.remove(tabs[0].id);
   }
 });
